53aa0c1289485b4c20f5014b0dba6a17219fcdb7
Tiago <tiago.cambara@visualsoft.co.uk>
2024-07-04 11:02:14 +0100

    feat(sc-190): add the pinia example

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <template>
   <main>
     HOME PAGE
+    {{ userName }}
   </main>
 </template>
+
+<script setup type="ts">
+import { userStore } from '@/stores/user'
+import { computed } from 'vue';
+
+const store = userStore()
+
+const userName = computed(() => store.userName);
+</script>
