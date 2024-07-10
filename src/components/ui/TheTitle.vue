162e34b50a5a628477fb5d915427298af6c74ef0
Tiago <tiago.cambara@visualsoft.co.uk>
2024-07-10 16:57:21 +0100

    feat(sc-197): implement the title component

---
+++ src/components/ui/TheTitle.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>
+        <slot></slot>
+    </h1>
+</template>
+
+<style scoped>
+h1 {
+    @apply font-mono text-3xl text-left underline decoration-cyan-400 decoration-4 underline-offset-8 mb-8;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,13 @@ import App from './App.vue'
 import router from './router'
 
 import TheContainer from './components/ui/TheContainer.vue'
+import TheTitle from './components/ui/TheTitle.vue'
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.component('TheContainer', TheContainer)
+app.component('TheTitle', TheTitle)
 
 app.use(pinia)
 app.use(router)
