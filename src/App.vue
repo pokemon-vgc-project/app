291cfdd304c7cbb7079a702a5fe80271b6c1deb2
Tiago <tiago.cambara@visualsoft.co.uk>
2024-07-10 13:04:00 +0100

    feat(sc-197): add TheHeader component into App

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import TheHeader from '@/components/nav/TheHeader.vue'
 </script>
 
 <template>
-  <RouterView />
+  <TheHeader />
+  <section class="content">
+    <RouterView />
+  </section>
 </template>
+
+<style scoped>
+.content {
+  @apply relative top-20;
+}
+</style>
