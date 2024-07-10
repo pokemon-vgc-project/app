9ae964b553e8626f1307d3c6cd7b315c00193335
Tiago <tiago.cambara@visualsoft.co.uk>
2024-07-10 16:42:39 +0100

    feat(sc-197): implement the container component

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import TheContainer from './components/ui/TheContainer.vue'
+
 const pinia = createPinia()
 const app = createApp(App)
+
+app.component('TheContainer', TheContainer)
 
 app.use(pinia)
 app.use(router)
