db961796bc6583338880edc90449940b48054f27
Tiago <tiago.cambara@visualsoft.co.uk>
2024-07-10 17:29:26 +0100

    feat(sc-197): implement the natures' table layout

--- src/views/Natures/NaturesView.vue
+++ src/views/Natures/NaturesView.vue
@@ -1,9 +1,53 @@
 <template>
-    <section>
-        <h1>Natures</h1>
-        TABLE
-    </section>
+    <TheContainer>
+        <TheTitle>Natures</TheTitle>
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Increase</th>
+                    <th>Decrease</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>test</td>
+                    <td>test</td>
+                    <td>test</td>
+                </tr>
+                <tr>
+                    <td>test</td>
+                    <td>test</td>
+                    <td>test</td>
+                </tr>
+            </tbody>
+        </table>
+    </TheContainer>
 </template>
 
-<script setup lang="ts">
-</script>
+<style scoped>
+table {
+    @apply table-auto border-collapse border w-full;
+}
+
+table th {
+    @apply text-lg font-bold bg-red-500 text-white;
+}
+
+table td {
+    @apply text-sm;
+}
+
+table tbody td:first-child {
+    @apply text-lg font-medium;
+}
+
+table tbody tr:hover {
+    @apply bg-cyan-400 text-stone-50 duration-300;
+}
+
+table th,
+table td {
+    @apply border-b text-center p-4;
+}
+</style>
