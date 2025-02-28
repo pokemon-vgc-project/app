02fe6831e767b46584aa2c734d824c46e8ee8cb4
Tiago Cambará <tiago.cambara@parceiros.nos.pt>
2025-02-28 14:21:35 +0000

    feat(sc-223): implement a method to get the pokemons

---
+++ src/services/mappers/pokemon_parameters.mapper.ts
@@ -0,0 +1,45 @@
+export interface FilterStartEndModel {
+    start?: number;
+    end?: number;
+}
+
+interface GetPokemonsParametersSort {
+    name: string;
+    order?: 'asc' | 'desc';
+}
+
+interface GetPokemonsParametersFilters {
+    forms?: string[];
+    types?: number[];
+    abilities?: number[];
+    name?: string;
+    pokemonNum?: FilterStartEndModel;
+    heightm?: FilterStartEndModel;
+    weightkg?: FilterStartEndModel;
+}
+export interface GetPokemonsParameters {
+    limit?: number;
+    skip?: number;
+    sorts?: GetPokemonsParametersSort[];
+    filters?: GetPokemonsParametersFilters;
+}
+
+export const pokemonParametersToURLSearchParams = (dto: GetPokemonsParameters): URLSearchParams => {
+    const params = new URLSearchParams();
+    if (dto.limit) {
+        params.append('limit', dto.limit.toString());
+    }
+    if (dto.skip) {
+        params.append('skip', dto.skip.toString());
+    }
+    if (dto.sorts) {
+        dto.sorts.forEach((sort) => {
+            params.append('sorts[]', JSON.stringify(sort));
+        });
+    }
+    if (dto.filters) {
+        params.append('filters', JSON.stringify(dto.filters));
+    }
+    return params;
+
+};
--- src/services/pokemon.api.ts
+++ src/services/pokemon.api.ts
@@ -1,6 +1,10 @@
 import type { Nature } from "@/dto/nature.interface";
+import type { Pokemon } from "@/dto/pokedex.interface";
 import type { BasePaginationResponse } from "@/dto/pokedex_api.interface";
 import type { Api } from "@/infra/api/api.interface";
+import { pokemonParametersToURLSearchParams, type GetPokemonsParameters } from "./mappers/pokemon_parameters.mapper";
+
+
 
 export class PokemonApi {
     constructor(private readonly api: Api) { }
@@ -8,6 +12,15 @@ export class PokemonApi {
     async getNatures(): Promise<BasePaginationResponse<Nature[]>> {
         return this.api.getRequest<BasePaginationResponse<Nature[]>>({
             router: '/natures',
+        })
+    }
+
+    async getPokemons(dto: GetPokemonsParameters = {
+        limit: 10,
+    }): Promise<BasePaginationResponse<Pokemon[]>> {
+        return this.api.getRequest<BasePaginationResponse<Pokemon[]>>({
+            router: '/pokemons',
+            parameters: pokemonParametersToURLSearchParams(dto),
         })
     }
 }
